6659b8b8de0bdb552b9159a8600a451cfa6df3e6
Andrew <Andrei_xoma@mail.ru>
2023-11-17 05:23:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-document.getElementById('playerName').addEventListener('keypress', function (e) {
-  if (e.key === 'Enter') {
-    searchPlayer();
-  }
-});
-
 function searchPlayer() {
   var playerName = document.getElementById('playerName').value;
   var apiUrl = "https://api.pubg.com/shards/steam/players?filter[playerNames]=" + playerName;
 
   var xhr = new XMLHttpRequest();
   xhr.open("GET", apiUrl, true);
-  xhr.setRequestHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIyOTcyODQ4MC02NzJjLTAxM2MtYzM4YS0xYTljOTg2NWFhMWIiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNzAwMTkzNzk3LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Ii1mODI5ZmQ2Ni04ZGE1LTQ3ZWMtODU0YS01MjUwZTFhZDA4NGMifQ.0iUaoOI6xc_oIc2IVbYp3JLbkrhWgBr8JbmND6J4e3o"); // Replace YOUR_ACCESS_TOKEN with your actual token
+  xhr.setRequestHeader("Authorization", "Bearer YOUR_ACCESS_TOKEN"); // Replace YOUR_ACCESS_TOKEN with your actual token
   xhr.setRequestHeader("Accept", "application/vnd.api+json");
 
   xhr.onreadystatechange = function () {
@@ -29,7 +23,7 @@ function searchPlayer() {
 
 function displayPlayerData(playerData) {
   var playerInfo = document.getElementById('playerData');
-  playerInfo.innerHTML = ''; // Clear previous result (if any)
+  playerInfo.innerHTML = '';
 
   if (playerData.data && playerData.data.length > 0) {
     var playerHTML = "<h2>Player Information:</h2>";
@@ -50,7 +44,7 @@ function displayPlayerData(playerData) {
 
     playerInfo.innerHTML = playerHTML;
   } else {
-    displayError("Player not found");
+    displayError("No Players Found Matching Criteria");
   }
 }
 
